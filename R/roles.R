02f0083bbf9d14e7d76df8a640a0f3c77966f83f
Alex Gold <alexkgold@gmail.com>
2019-07-23 15:53:11 -0400

    What submitted to CRAN

--- R/roles.R
+++
@@ -1,9 +0,0 @@
-# Wrappers for roles API
-
-ol_roles_get <- function(con, ...) {
-  con$GET("api/1/roles", ...)
-}
-
-ol_role_get_by_id <- function(con, role_id) {
- con$GET(glue::glue("api/1/roles/{role_id}"))
-}
